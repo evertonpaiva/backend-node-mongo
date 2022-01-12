521571b397a65a87573841b0eb535f941ce21ca6
Everton Paiva <everton.paiva@ufvjm.edu.br>
2022-01-12 16:45:21 -0300

    Adicionando rota do controler de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const UsuarioController = require('./controller/UsuarioController');
 
 const routes = express.Router();
 
 routes.get('/', function(req, res){
     res.json({message: "Bem vindo ao Back End Mongo DB"});
 });
+
+routes.get('/usuario', UsuarioController.index);
 
 module.exports = routes;
